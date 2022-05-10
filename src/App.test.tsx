cd120e064dcd6bac73936b434c4516ac2aac8c27
Alang Kim <rlaangh77@naver.com>
2022-05-11 04:31:01 +0900

    feat: render test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders header1", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Alang Blog/i);
   expect(linkElement).toBeInTheDocument();
 });
