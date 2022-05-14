1049106e5a29b454432112c740b5ed425de246f4
Alang Kim <rlaangh77@naver.com>
2022-05-14 22:58:23 +0900

    ✨ feat: colors constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+const colors = {
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: "#ACAC9A",
+  primary: "#F9FDC9",
+  green: "#BADBB4",
+  blue: "#5C918F",
+  navy: "#2F4858",
+} as const;
+
+export default colors;
