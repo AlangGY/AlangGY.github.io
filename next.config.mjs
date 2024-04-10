b208cca1ec99251bfafc506f501820e38b6bc3f4
Alang Kim <rlaangh77@naver.com>
2024-04-10 17:02:38 +0900

    Config: BundleAnayzer 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
 import withMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withMDX()({
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  images: { unoptimized: true },
-  basePath: "",
-  output: "export",
-});
+const nextConfig = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(
+  withMDX()({
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    images: { unoptimized: true },
+    basePath: "",
+    output: "export",
+  })
+);
 
 export default nextConfig;
